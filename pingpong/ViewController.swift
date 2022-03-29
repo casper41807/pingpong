636d1690cdd9b8a080b495cae357b5039b451b6e
casper41807 <casper41807@gmail.com>
2022-03-29 18:45:02 +0800

    reset

--- pingpong/ViewController.swift
+++ pingpong/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var leftScoreOutlet: UIButton!
     @IBOutlet weak var rightScoreOutlet: UIButton!
     
+    @IBOutlet weak var changeSideOutlet: UIButton!
+    
     @IBOutlet var viewArray: [UIView]!
     
     var num:Int?
@@ -33,17 +35,21 @@ class ViewController: UIViewController {
     
     var leftTotalScore = 0
     var rightTotalScore = 0
-     
-    
-    
+    //點空白處離開鍵盤
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+            view.endEditing(true)
+        }
+    //畫面只提供橫向
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+            .landscape
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         resetGame()
-       
-        
     }
+    //得分Button
     @IBAction func ScoreButton(_ sender: UIButton) {
         switch sender.tag{
         case 0 :
@@ -87,7 +93,6 @@ class ViewController: UIViewController {
             rightTotalScoreLable.text = "\(rightTotalScore)"
             clearScore()
             winnerExchangeBall()
-            
             }
         default:
             if leftScore > 9,rightScore > 9{
@@ -95,8 +100,10 @@ class ViewController: UIViewController {
             }
         }
     }
-    
-    
+    //按return離開鍵盤
+    @IBAction func NameEnd(_ sender: UITextField) {
+    }
+    //開始比賽
     @IBAction func startButton(_ sender: UIButton) {
         if let textfield = leftNameTextField.text,let textfield2 = rightNameTextField.text{
             leftTeamLabel.text = textfield
@@ -107,6 +114,7 @@ class ViewController: UIViewController {
             rightNameTextField.isHidden = true
             leftScoreOutlet.isEnabled = true
             rightScoreOutlet.isEnabled = true
+            changeSideOutlet.isEnabled = true
             viewArray[0].isHidden = true
             viewArray[1].isHidden = true
             let index = Int.random(in: 0...1)
@@ -118,27 +126,13 @@ class ViewController: UIViewController {
             if rightTeamLabel.text == ""{
                 rightTeamLabel.text = "Player 2"
             }
-            
         }
     }
-    
-
-    
+    //換邊Button
     @IBAction func ChangeSideButton(_ sender: UIButton) {
-
+        ChangeSide()
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //全部清空
+    //全部清空Button
     @IBAction func resetButton(_ sender: UIButton) {
         clearScore()
         rightTotalScoreLable.text = "0"
@@ -146,7 +140,6 @@ class ViewController: UIViewController {
         rightTotalScore = 0
         leftTotalScore = 0
         resetGame()
-       
     }
     //清空得分
     func clearScore(){
@@ -171,7 +164,7 @@ class ViewController: UIViewController {
             winnerExchangeBall()
         }
     }
-    //輪替發球
+    //輪替換發
     func exchangeBall(){
         let sum = leftScore + rightScore
         if viewArray[0].isHidden == true{
@@ -192,7 +185,7 @@ class ViewController: UIViewController {
             }
         }
     }
-    //deuce輪替發球
+    //deuce輪替換發
     func deuceExchangeBall(){
         if viewArray[0].isHidden == true{
                 viewArray[0].isHidden = false
@@ -204,7 +197,42 @@ class ViewController: UIViewController {
     }
     //交換位置
     func ChangeSide(){
+        let exchangeLeftScore = leftScore
+        let exchangeRightScore = rightScore
         
+        let exchangeLeftScoreLabel = leftScoreLabel.text
+        let exchangeRightScoreLabel = rightScoreLable.text
+       
+        let exchangeLeftTotalScore = leftTotalScore
+        let exchangeRightTotalScore = rightTotalScore
+        
+        let exchangeLeftTotalScoreLabel = leftTotalScoreLable.text
+        let exchangeRightTotalScoreLabel = rightTotalScoreLable.text
+        
+        let leftTeam = leftTeamLabel.text
+        let rightTeam = rightTeamLabel.text
+        
+        leftScore = exchangeRightScore
+        rightScore = exchangeLeftScore
+        
+        leftScoreLabel.text = exchangeRightScoreLabel
+        rightScoreLable.text = exchangeLeftScoreLabel
+        
+        leftTotalScore = exchangeRightTotalScore
+        rightTotalScore = exchangeLeftTotalScore
+        
+        leftTotalScoreLable.text = exchangeRightTotalScoreLabel
+        rightTotalScoreLable.text = exchangeLeftTotalScoreLabel
+        
+        leftTeamLabel.text = rightTeam
+        rightTeamLabel.text = leftTeam
+        if viewArray[0].isHidden == true {
+            viewArray[0].isHidden = false
+            viewArray[1].isHidden = true
+        }else{
+            viewArray[0].isHidden = true
+            viewArray[1].isHidden = false
+        }
     }
     //重設遊戲
     func resetGame(){
@@ -218,21 +246,20 @@ class ViewController: UIViewController {
         viewArray[1].isHidden = true
         leftScoreOutlet.isEnabled = false
         rightScoreOutlet.isEnabled = false
+        changeSideOutlet.isEnabled = false
     }
-    
+    //輪流發球
     func winnerExchangeBall(){
         if num == 1{
             viewArray[0].isHidden = false
             viewArray[1].isHidden = true
+            num = 0
         }else if num == 0{
             viewArray[0].isHidden = true
             viewArray[1].isHidden = false
+            num = 1
         }
     }
-    
-    
-    
-    
     
 }
 
